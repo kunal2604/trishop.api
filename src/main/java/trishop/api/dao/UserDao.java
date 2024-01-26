b27492b8d9aee39301c432bab7240336aa61a536
Kunal Kumar <kmrknl2604@gmail.com>
2024-01-26 19:02:59 +0530

    Revert "Renaming - 1"
    This reverts commit ab2a1c43d0760f66baefd083b91be047a1cfc08e.

--- src/main/java/trishop/api/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package trishop.api.dao;
-
-import trishop.api.entity.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDao extends CrudRepository<User, String> {
-}
