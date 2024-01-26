92e2cf550b8c40280e76086c93d7ac242962bbea
Kunal Kumar <kmrknl2604@gmail.com>
2024-01-27 04:03:19 +0530

    API for uploading image

--- src/main/java/trishop/api/controller/ProductController.java
+++ src/main/java/trishop/api/controller/ProductController.java
@@ -1,18 +1,49 @@
 package trishop.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import trishop.api.entity.ImageModel;
 import trishop.api.entity.Product;
 import trishop.api.service.ProductService;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 public class ProductController {
     @Autowired
     private ProductService _productService;
-    @PostMapping({"/product/add"})
-    public Product addNewProduct(@RequestBody Product newProduct) {
-        return _productService.addNewProduct(newProduct);
+    @PostMapping(value = {"/product/add"}, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public Product addNewProduct(@RequestPart("product") Product newProduct,
+                                 @RequestPart("imageFile")MultipartFile[] files) {
+        try {
+            Set<ImageModel> uploadedImages = uploadImage(files);
+            newProduct.setProductImages(uploadedImages);
+            return _productService.addNewProduct(newProduct);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
+
+    public Set<ImageModel> uploadImage(MultipartFile[] files) throws IOException {
+        Set<ImageModel> imageModels = new HashSet<>();
+        for(MultipartFile file : files) {
+            ImageModel imageModel = new ImageModel(
+                    file.getOriginalFilename(),
+                    file.getContentType(),
+                    file.getBytes()
+            );
+            imageModels.add(imageModel);
+        }
+        return imageModels;
+    }
+
 }
--- src/main/java/trishop/api/entity/Product.java
+++ src/main/java/trishop/api/entity/Product.java
@@ -1,9 +1,6 @@
 package trishop.api.entity;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Product {
@@ -15,8 +12,25 @@ public class Product {
     private Double price;
     private Double discount;
 
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "product_images",
+        joinColumns = {
+            @JoinColumn(name = "product_id")
+        }, inverseJoinColumns = {
+            @JoinColumn(name = "image_id")
+        }
+    )
+    private Set<ImageModel> productImages;
     public Integer getProductId() {
         return productId;
+    }
+
+    public Set<ImageModel> getProductImages() {
+        return productImages;
+    }
+
+    public void setProductImages(Set<ImageModel> productImages) {
+        this.productImages = productImages;
     }
 
     public void setProductId(Integer productId) {
