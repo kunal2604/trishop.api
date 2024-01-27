789b7bc3fa8440567fb8fd3a735272dd16b2bb61
Kunal Kumar <kmrknl2604@gmail.com>
2024-01-28 03:19:04 +0530

    Remove unused line

--- src/main/java/trishop/api/controller/ProductController.java
+++ src/main/java/trishop/api/controller/ProductController.java
@@ -3,7 +3,6 @@ package trishop.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
